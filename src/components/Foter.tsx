5241131f13c99359e5f3744c38648a2275392afc
sunam007 <asaduzzaman.sunam@gmail.com>
2024-10-24 21:59:54 +0600

    project initialized

---
+++ src/components/Foter.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ background: '#333', color: '#fff', textAlign: 'center', padding: '10px 20px' }}>
+            <p>© 2024 Your Website</p>
+        </footer>
+    );
+}
+
+export default Footer;
